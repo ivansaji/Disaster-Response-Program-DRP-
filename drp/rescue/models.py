95c3c938307499f5705947f022ef8ee59ac5b1ef
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2019-07-15 21:19:54 +0530

    Template setup

--- drp/rescue/models.py
+++ drp/rescue/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Refugee(models.Model):
+    name = models.CharField(max_length = 25)
+    contact = models.CharField(max_length=10)
+    pub_time = models.DateTimeField(auto_now_add=True)
+    location = models.TextField()
+    threat_rating = models.IntegerField()
+    necessities = models.TextField()
+    no_of_people = models.IntegerField()
+
+class Volunteer(models.Model):
+    name = models.CharField(max_length=25)
+    contact = models.CharField(max_length=10)
+    pub_time = models.DateTimeField(auto_now_add=True)
